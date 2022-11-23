89636aab7a91c25c51c28775e17074f9dbbb5b44
slazarska <slazarska@protonmail.com>
2022-11-23 13:57:08 +0300

    added in conftest setting webdriver

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selene.support.shared import browser
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def set_webdriver():
+    browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
+    return browser.config.driver
+
+
+@pytest.fixture()
+def set_browser_size():
+    browser.config.driver.maximize_window()
+
+
+@pytest.fixture()
+def open_browser(set_webdriver, set_browser_size):
+    browser.open('https://google.com/ncr')
